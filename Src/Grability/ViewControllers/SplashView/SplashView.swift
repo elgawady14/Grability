824405c026f019d1c2712c9dd6928028e4f501c8
Ahmad Abdul-Gawad Mahmoud <AhmadAbdulGawadMahmoud@idisk-pro.local>
2016-03-19 14:36:29 +0200

    Project construction  continued :)

---
+++ Src/Grability/ViewControllers/SplashView/SplashView.swift
@@ -0,0 +1,35 @@
+//
+//  SplashView.swift
+//  Grability
+//
+//  Created by Ahmad Abdul-Gawad Mahmoud on 3/19/16.
+//  Copyright © 2016 Ahmad Abdul-Gawad Mahmoud. All rights reserved.
+//
+
+import UIKit
+
+class SplashView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
