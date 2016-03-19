3b449506c185d267c6650806497516193b11249d
Ahmad Abdul-Gawad Mahmoud <AhmadAbdulGawadMahmoud@idisk-pro.local>
2016-03-19 14:17:49 +0200

    Initial commit.

---
+++ Src/Grability/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Grability
+//
+//  Created by Ahmad Abdul-Gawad Mahmoud on 3/19/16.
+//  Copyright © 2016 Ahmad Abdul-Gawad Mahmoud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
