590f2c66377cd3bc3b6150c90d01b0a98bdfc9cc
Ahmad Abdul-Gawad Mahmoud <AhmadAbdulGawadMahmoud@idisk-pro.local>
2016-03-22 21:36:49 +0200

    Test scroll is working in App summary view :)

---
+++ Src/Grability/ViewControllers/AppSummary/AppSummary.swift
@@ -0,0 +1,38 @@
+//
+//  AppSummary.swift
+//  Grability
+//
+//  Created by Ahmad Abdul-Gawad Mahmoud on 3/22/16.
+//  Copyright © 2016 Ahmad Abdul-Gawad Mahmoud. All rights reserved.
+//
+
+import UIKit
+
+class AppSummary: BaseViewController {
+
+    @IBOutlet var lab: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        lab.text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
